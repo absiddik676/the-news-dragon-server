bed51d7690be7c87fff25968d67a083a3dc97de2
absiddik676 <absiddik8200@gmail.com>
2023-05-02 00:51:48 +0600

    vercal added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 const cors = require('cors')
 const category = require('./Data/categorys.json')
+const newses = require('./Data/news.json')
 
 app.use(cors())
 
@@ -10,8 +11,26 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/categorys',(req,res)=>{
-    res.send(category)
+app.get('/categorys', (req, res) => {
+  res.send(category)
+})
+app.get('/news', (req, res) => {
+  res.send(newses)
+})
+app.get('/news/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedNewses = newses.find(news => news._id === id);
+  res.send(selectedNewses)
+})
+app.get('/categorys/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log(id);
+  if (id == 0) {
+    res.send(newses)
+  } else {
+    const categoryNews = newses.filter(news => parseInt(news.category_id) == id);
+    res.send(categoryNews)
+  }
 })
 
 app.listen(port, () => {
